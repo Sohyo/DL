b8eb7fe93fb232ba6e3c827507fc01a2269b8471
Sohyo <42966239+Sohyo@users.noreply.github.com>
2019-04-25 15:29:20 +0200

    Add files via upload

---
+++ resnet_try.py
@@ -0,0 +1,119 @@
+
+import torchvision.models as models
+from torchvision.models.resnet import ResNet, BasicBlock
+from torchvision.datasets import CIFAR100
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+import inspect
+import time
+from torch import nn, optim
+import torch
+from torchvision.transforms import Compose, ToTensor, Normalize, Resize
+from torch.utils.data import DataLoader
+#from tqdm.autonotebook import tqdm
+
+
+def get_data_loaders(train_batch_size, val_batch_size):
+    # Transform function first Resize -> toTensor -> then normalize pixel values
+    data_transform = Compose([ Resize((224, 224)),ToTensor(), Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
+        
+    train_loader = DataLoader(CIFAR100(download=True, root="./data", transform=data_transform, train=True),
+                              batch_size=train_batch_size, shuffle=True)
+
+    val_loader = DataLoader(CIFAR100(download=True, root="./data", transform=data_transform, train=False),
+                            batch_size=val_batch_size, shuffle=False)
+    return train_loader, val_loader
+
+def calculate_metric(metric_fn, true_y, pred_y):
+    # multi class problems need to have averaging method
+    if "average" in inspect.getfullargspec(metric_fn).args:
+        return metric_fn(true_y, pred_y, average="macro")
+    else:
+        return metric_fn(true_y, pred_y)
+    
+def print_scores(p, r, f1, a, batch_size):
+    # just an utility printing function
+    for name, scores in zip(("precision", "recall", "F1", "accuracy"), (p, r, f1, a)):
+        print(f"\t{name.rjust(14, ' ')}: {sum(scores)/batch_size:.4f}")
+
+
+# model:
+model = models.resnet18(pretrained=False, num_classes=100)
+
+# params you need to specify:
+epochs = 10
+# put your data loader here
+train_loader, val_loader = get_data_loaders(100, 100)
+loss_function = nn.CrossEntropyLoss() # your loss function, cross entropy works well for multi-class problems
+
+# optimizer, I've used Adadelta, as it wokrs well without any magic numbers
+optimizer = optim.Adadelta(model.parameters())
+
+start_ts = time.time()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+losses = []
+batches = len(train_loader)
+val_batches = len(val_loader)
+print("batchs: {}, val_batches: {}".format(batches, val_batches))
+
+# loop for every epoch (training + evaluation)
+for epoch in range(epochs):
+    total_loss = 0
+
+    # progress bar (works in Jupyter notebook too!)
+    #progress = tqdm(enumerate(train_loader), desc="Loss: ", total=batches)
+
+    # ----------------- TRAINING  -------------------- 
+    # set model to training
+    model.train()
+    
+    for i, data in enumerate(train_loader):
+        X, y = data[0].to(device), data[1].to(device)
+        
+        # training step for single batch
+        model.zero_grad()
+        outputs = model(X)
+
+        loss = loss_function(outputs, y)
+        loss.backward()
+        optimizer.step()
+
+        # getting training quality data
+        current_loss = loss.item()
+        total_loss += current_loss
+
+        # updating progress bar
+        #progress.set_description("Loss: {:.4f}".format(total_loss/(i+1)))
+        print(total_loss/(i+1))
+        
+    # releasing unceseccary memory in GPU
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+    
+    # ----------------- VALIDATION  ----------------- 
+    val_losses = 0
+    precision, recall, f1, accuracy = [], [], [], []
+    
+    # set model to evaluating (testing)
+    model.eval()
+    with torch.no_grad():
+        for i, data in enumerate(val_loader):
+            X, y = data[0].to(device), data[1].to(device)
+
+            outputs = model(X) # this get's the prediction from the network
+
+            val_losses += loss_function(outputs, y)
+
+            predicted_classes = torch.max(outputs, 1)[1] # get class from network's prediction
+            
+            # calculate P/R/F1/A metrics for batch
+            for acc, metric in zip((precision, recall, f1, accuracy), 
+                                   (precision_score, recall_score, f1_score, accuracy_score)):
+                acc.append(
+                    calculate_metric(metric, y.cpu(), predicted_classes.cpu())
+                )
+          
+    print(f"Epoch {epoch+1}/{epochs}, training loss: {total_loss/batches}, validation loss: {val_losses/val_batches}")
+    print_scores(precision, recall, f1, accuracy, val_batches)
+    losses.append(total_loss/batches) # for plotting learning curve
+print(f"Training time: {time.time()-start_ts}s")
