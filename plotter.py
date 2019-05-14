2629ac9e7c7a7970fd74556d93cde333aefe058d
Sohyo <cienakim12@gmail.com>
2019-05-14 21:32:10 +0200

    make new_jobscript In this file, there are 8 different combination of models
    Every epochs are 400
    
    resNet - fine tuning - adadelta
    resNet - fine tuning - adam
    resNet - feature extraction - adadelta
    resNet - feature extraction - adam
    
    denseNet - fine tuning - adadelta
    denseNet - fine tuning - adam
    denseNet - feature extraction - adadelta
    denseNet - feature extraction - adam

--- plotter.py
+++ plotter.py
@@ -10,7 +10,7 @@ def parse_arguments():
     parser.add_argument('--filename', type=str, default='default',
                         help='The base filename of the files we want to read in.')
     parser.add_argument('--everything', default=False, action='store_true')
-    
+
 
     args = parser.parse_args()
 
@@ -18,49 +18,78 @@ def parse_arguments():
 
 
 def read_in_files_to_average(filename, amount_of_files):
-	summed_data = None
-	for file_number in range(amount_of_files):
-		with open("{}_{}".format(filename, file_number), 'r') as f:
-			data = np.array(list(csv.reader(f))[1:]).astype(float)
-			if summed_data is None:
-				summed_data = np.zeros(data.shape)
-			summed_data += data
-	return summed_data / amount_of_files
+    summed_data = None
+    for file_number in range(amount_of_files):
+        with open("{}_{}".format(filename, file_number), 'r') as f:
+            data = np.array(list(csv.reader(f))[1:]).astype(float)
+            if summed_data is None:
+                summed_data = np.zeros(data.shape)
+            summed_data += data
+    averages = summed_data / amount_of_files
+
+    summed_data = None
+    for file_number in range(amount_of_files):
+        with open("{}_{}".format(filename, file_number), 'r') as f:
+            data = np.array(list(csv.reader(f))[1:]).astype(float)
+            data = np.square(data-averages)
+            if summed_data is None:
+                summed_data = np.zeros(data.shape)
+            summed_data += data
+    stds = np.sqrt(summed_data/amount_of_files)
+    return averages, stds
+
 
 def plot_accuracy(averages):
-	plt.plot(averages[:,5])
-	plt.ylabel('Accuracy')
-	plt.xlabel('Epochs')
-	plt.show()
+    plt.plot(averages[:,5])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epochs')
+    plt.show()
+
 
 def plot_losses(averages):
-	plt.plot(averages[:,0])
-	plt.plot(averages[:,1])
-	plt.ylabel('Loss')
-	plt.xlabel('Epochs')
-	plt.show()
+    plt.plot(averages[:,0])
+    plt.plot(averages[:,1])
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.show()
+
 
 def plot_multiple_accuracies(list_of_averages, filenames):
-	for averages in list_of_averages:
-		plt.plot(averages[:,5])
-	plt.ylabel('Accuracy')
-	plt.xlabel('Epochs')
-	plt.legend(filenames, loc='bottom right')
-	plt.show()
+    for averages in list_of_averages:
+        plt.plot(averages[:,5])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend(filenames, loc='bottom right')
+    plt.show()
 
 
 def plot_everything():
-	filenames = ["{}_{}_400_{}".format(net, method, optimizer) for net in ['dense', 'res'] for method in ['fe', 'ft'] for optimizer in ['adam', 'adadelta']]
-	all_averages = [read_in_files_to_average(filename, 4) for filename in filenames]
-	plot_multiple_accuracies(all_averages, filenames)
+    filenames = ["results/{}_{}_400_{}".format(net, method, optimizer) for net in ['dense', 'res'] for method in ['fe', 'ft'] for optimizer in ['adam', 'adadelta']]
+    all_averages = [read_in_files_to_average(filename, 4)[0] for filename in filenames]
+    plot_multiple_accuracies(all_averages, filenames)
+
+    # Print final accuracies + stds
+    print("\t\t\t\ttraining_loss\tvalidation_loss\tprecision\trecall\t\tf1\t\taccuracy")
+    averages_and_stds = [read_in_files_to_average(filename, 4) for filename in filenames]
+    for filename, (averages, stds) in zip(filenames, averages_and_stds):
+        s = filename + "\t" + ("\t" if "adam" in filename and "res_" in filename else "")
+        for average, std in zip(averages[-1,:], stds[-1,:]):
+            s += "%0.3f+/-%0.3f\t" % (average, std) #""{.3f}:{}\t".format(float(average), std)
+        print(s)
+
+
+def print_everything():
+    filenames = ["results/{}_{}_400_{}".format(net, method, optimizer) for net in ['dense', 'res'] for method in ['fe', 'ft'] for optimizer in ['adam', 'adadelta']]
+    all_averages = [read_in_files_to_average(filename, 4)[0] for filename in filenames]
+    print(all_averages)
+
 
 if __name__ == '__main__':
     amount_of_files, filename, everything = parse_arguments()
+
     if everything:
         plot_everything()
     else:
-        averages = read_in_files_to_average(filename, amount_of_files)
+        averages = read_in_files_to_average(filename, amount_of_files)[0]
         plot_accuracy(averages)
         plot_losses(averages)
-	
-#training_loss,validation_loss,precision,recall,f1,accuracy
